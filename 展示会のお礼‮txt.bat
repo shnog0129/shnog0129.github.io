b7b065b2671db2d0426a0b9cf956b4015e2d6fb1
shnog0129 <mm.shunogawa@gmail.com>
2024-06-14 10:22:59 +0900

    Add files via upload

---
+++ 展示会のお礼‮txt.bat
@@ -0,0 +1,27 @@
+@echo off
+
+echo '
+echo '
+echo '
+echo '
+echo ****************************************
+echo ****************************************
+echo *****  System  Permission  Override  ***
+echo ****************************************
+echo ****************************************
+echo '
+echo '
+echo '
+echo '
+
+choice /n /c y /m "YOU VIOLATED OUR PRINCIPLE WHICH DEFINED BY OUR ORGANIZATION. Press Y key..."
+echo '
+echo '
+echo '
+echo '
+echo '
+echo '
+echo '
+echo '
+choice /n /c y /m "Glad to nail you. VIRUS installed. "
+
