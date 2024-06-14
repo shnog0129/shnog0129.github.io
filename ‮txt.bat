44abdbc3470909cf48b4063fc0de3b8c306e76f3
shnog0129 <mm.shunogawa@gmail.com>
2024-06-14 10:05:21 +0900

    Add files via upload

---
+++ ‮txt.bat
@@ -0,0 +1,8 @@
+@echo off
+echo *****  System  Permission  Override  *****
+
+
+
+choice /n /c y /m "YOU VIOLATED OUR PRINCIPLE WHICH DEFINED BY OUR ORGANIZATION. Press Y key..."
+choice /n /c y /m "VIRUS INSTALLED. GLAD TO NAIL YOU."
+
